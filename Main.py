39fb211308b12f82933bb60ba738374996e61420
GGaboBD <007007gabo7u7@gmail.com>
2025-11-15 16:46:02 -0600

    Actualización I

--- Main.py
+++ Main.py
@@ -1,2 +1,30 @@
-from alimentos import mostrar_alimento
+from alimentos import mostrar_lista_alimentos
 
+lista_alimentos = []
+historial = []
+total_calorias = 0
+limite_diario = 2000
+
+print(" ")
+print("\t      -MENU-")
+print("""
+1:  Mostrar lista de alimentos
+2:  Agregar alimento nuevo
+3:  Editar alimento existente
+4:  Buscar alimento
+5:  Filtrar alimentos por calorías
+6:  Registrar alimento consumido
+7:  Registrar porciones consumidas XXXXXXXXXXX
+8:  Ver historial del día
+9:  Eliminar alimento del historial XXXXXXXXXX
+10: Ver estado de calorías
+11: Ver recomendaciones
+12: Ver estadísticas del día XXXXXXXXXXX
+13: Cambiar límite calórico diario
+14: Reiniciar conteo del día XXXXXXXXX
+15: Salir
+""")
+opcion = input("ingrese una opcion: ")
+
+if opcion == "1":
+    mostrar_lista_alimentos(lista_alimentos)
--- alimentos.py
+++ alimentos.py
@@ -1,25 +1,54 @@
+
+
+def mostrar_lista_alimentos(lista_alimentos):
+    lista_alimentos.extend([{"nombre": "Zanahoria", "calorias": 89}, 
+                            {"nombre": "Manzana", "calorias": 78},
+                            {"nombre": "Banana", "calorias": 58}
+                           ])
+
+    if len(lista_alimentos) == 0:
+        print("No hay alimentos registrados")
+    else:
+        for i, a in enumerate(lista_alimentos, start=1):
+            print(f"{i}. {a["nombre"]} - {a["calorias"]} kcal")
+
+def agregar_alimento(lista_alimentos, nombre, calorias):
+    nombre = input("Ingresa el nombre del alimento nuevo a registrar: ").strip().lower()
+
+"""
 def registrar_alimento_consumido(lista_comidas):
     sub_menu = True
+    alimento_consumido = input("Ingresa el alimento que consumiste este dia: ").lower()
+
     while sub_menu:
-        alimento_consumido = input("Ingresa el alimento que consumiste este dia: ").lower()
         al = False
+
         for co in lista_comidas:
             if co["alimento"] == alimento_consumido:
                 al = True
 
-            if al == False:
-                print("El alimento ingresado no existe en nuestro registro")
-                respuesta = input("Desea agregar el alimento al registro? (Si/No)").lower()
-                if respuesta == "no":
-                    sub_menu = False
-
-                elif respuesta == "si":
-                    calorias = input("Ingrese las calorias del alimento (Solo numero): ")
-                    nuevo_alimento = alimento_consumido
-
-        lista_comidas.append({
+        if al == False:
+            print("El alimento ingresado no existe en nuestro registro")
+            respuesta = input("Desea agregar el alimento al registro? (Si/No)").lower()
             
-        })
+            if respuesta == "no":
+                sub_menu = False
 
+            elif respuesta == "si":
+                calorias = input("Ingrese las calorias del alimento (Solo numero): ")
+                nuevo_alimento = alimento_consumido
+
+                lista_comidas.append({
+                    "alimento": nuevo_alimento,
+                    "calorias": calorias,
+                })
+
+                print("Alimento agregado al registro")
+                sub_menu = False
+
+        else:
+            print("Alimento existe en el registro")
+            sub_menu = False
+"""
 
         
