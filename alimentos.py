29d08181f5dd3161e2bc6ce628f3124494bd1d10
GGaboBD <007007gabo7u7@gmail.com>
2025-11-15 09:05:36 -0600

    Alimentos

---
+++ alimentos.py
@@ -0,0 +1,8 @@
+def registrar_alimento(lista_comidas):
+    alimento_consumido = input("Ingresa el alimento que consumiste este dia: ").lower()
+    al = False
+    for co in lista_comidas:
+        if co["alimento"] == alimento_consumido:
+            al = True
+        if al == True
+    
