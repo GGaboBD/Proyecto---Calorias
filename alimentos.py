1de5bb8b50b0a7407db364deb5f5f640294a36ba
GGaboBD <007007gabo7u7@gmail.com>
2025-11-15 11:24:27 -0600

    Update alimentos.py

--- alimentos.py
+++ alimentos.py
@@ -1,8 +1,25 @@
-def registrar_alimento(lista_comidas):
-    alimento_consumido = input("Ingresa el alimento que consumiste este dia: ").lower()
-    al = False
-    for co in lista_comidas:
-        if co["alimento"] == alimento_consumido:
-            al = True
-        if al == True
-    
+def registrar_alimento_consumido(lista_comidas):
+    sub_menu = True
+    while sub_menu:
+        alimento_consumido = input("Ingresa el alimento que consumiste este dia: ").lower()
+        al = False
+        for co in lista_comidas:
+            if co["alimento"] == alimento_consumido:
+                al = True
+
+            if al == False:
+                print("El alimento ingresado no existe en nuestro registro")
+                respuesta = input("Desea agregar el alimento al registro? (Si/No)").lower()
+                if respuesta == "no":
+                    sub_menu = False
+
+                elif respuesta == "si":
+                    calorias = input("Ingrese las calorias del alimento (Solo numero): ")
+                    nuevo_alimento = alimento_consumido
+
+        lista_comidas.append({
+            
+        })
+
+
+        
